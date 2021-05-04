3bf88e7f24a68bbb3ad9813152489ef0ea982808
Andrei Hajdukewycz <sancus@off.net>
2021-05-04 15:47:12 -0700

    Refactor scalar queries to cache monthly user total.

--- tools/scalars.py
+++ tools/scalars.py
@@ -8,14 +8,15 @@ start_date = datetime.date(2021, 1, 3)
 filename = Path(__file__).parent / '../docs/telemetry.json'
 
 cached = tools.parse_cached_json(filename)
-for s in settings.keyedscalars:
-    dataloc = cached['json'].setdefault(s['key'], {})
-    for daystring in tools.date_range(start_date, 7):
+
+for daystring in tools.date_range(start_date, 7):
+    curdate = datetime.datetime.strptime(daystring, "%Y-%m-%d").date()
+    q = tools.AthenaQuery(curdate)
+    for s in settings.keyedscalars:
+        dataloc = cached['json'].setdefault(s['key'], {})
         if daystring not in dataloc:
             dataloc[daystring] = {}
             print(s['key'], daystring)
-            curdate = datetime.datetime.strptime(daystring, "%Y-%m-%d").date()
-            q = tools.AthenaQuery(curdate)
             d = q.keyedscalar_users(s['key']).format_data(s.get('combine', None)).json()
             dataloc[daystring] = d
         with open(filename, 'w') as file:
