222f618a1a92fe62188503e7dc9c4a5f3a3e93c7
Andrei Hajdukewycz <sancus@off.net>
2021-12-16 14:47:44 -0800

    Add JSON output to locale queries.

--- tools/platlang.py
+++ tools/platlang.py
@@ -32,4 +32,26 @@ class PlatLangUsers(tools.AthenaQuery):
             'count': self.totalusers
         }
 
+def run_locale_query(curdate, num_days):
+    return PlatLangUsers(curdate, num_days, settings.release_version).query_locales()
 
+def weekly(start_date, filename, func):
+    """ Run func once per week and store the data in JSON. """
+    # Number of days to query between, SQL Between is inclusive so this is 7 days of actual data.
+    num_days = 6
+    dataloc = tools.parse_cached_json(filename)['json']
+    for daystring in tools.date_range(start_date, num_days+1):
+        if daystring not in dataloc:
+            dataloc[daystring] = {}
+            print(daystring)
+            curdate = datetime.datetime.strptime(daystring, "%Y-%m-%d").date()
+            dataloc[daystring] = func(curdate, num_days)
+        with open(filename, 'w') as file:
+            json.dump(dataloc, file)
+
+# Number of users per locale code.
+locale_file = Path(__file__).parent / '../docs/locales.json'
+weekly(start_date, locale_file, run_locale_query)
+
+# Number of users per platform.
+platform_file = Path(__file__).parent / '../docs/platforms.json'
