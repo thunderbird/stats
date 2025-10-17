1f286aeba378a8b9170271b02a46788cedd52bc6
Andrei Hajdukewycz <sancus@off.net>
2025-10-17 02:13:34 +0000

    Update spending percentages for 2024.

--- docs/media/js/stats.js
+++ docs/media/js/stats.js
@@ -405,7 +405,7 @@ yearly_options = {
     yAxis: {title: {text: '$ US'}},
     labels: {
         items: [{
-            html: 'Expense Breakdown for 2023',
+            html: 'Expense Breakdown for 2024',
             style: {
                 left: '150px',
                 top: '20px',
